36d29cfbdb48cf4b324ff2c302d8865205a5e52c
IndianRenters <indianrenters@IndianRenterss-MacBook-Pro.local>
2023-08-11 11:56:12 +0530

    master

--- lib/screen/mcc_screen/dashboard_tab_screen/application_screen.dart
+++ lib/screen/mcc_screen/dashboard_tab_screen/application_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glad/screen/custom_widget/custom_appbar.dart';
 import 'package:glad/screen/custom_widget/custom_methods.dart';
+import 'package:glad/utils/extension.dart';
+import 'package:glad/utils/images.dart';
+import 'package:glad/utils/styles.dart';
 
 import 'application_widget.dart';
 
@@ -8,10 +13,98 @@ class ApplicationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return customList(child: (index){
-      return const Padding(
-        padding: EdgeInsets.only(right: 20.0,left: 10),
-        child: MCCApplicationScreen(bottom: 25,),
-      );});
+    return Column(
+      children: [
+
+        CustomAppBar(
+          context: context,
+          titleText1: 'Loan Applications',
+          titleText1Style: figtreeMedium.copyWith(
+              fontSize: 20, color: Colors.black),
+          centerTitle: true,
+          leading: openDrawer(
+              onTap: () {
+                // farmerLandingKey.currentState?.openDrawer();
+              },
+              child: SvgPicture.asset(Images.arrowBack)),
+        ),
+
+        10.verticalSpace(),
+
+
+        Container(
+          height: 50,
+          margin: 20.marginHorizontal(),
+          width: screenWidth(),
+          decoration: boxDecoration(
+              borderRadius: 62,
+              borderColor: const Color(0xffDCDCDC)
+          ),
+          child: Row(
+            children: [
+
+              Expanded(
+                child: Container(
+                  height: screenHeight(),
+                  margin: const EdgeInsets.all(6),
+                  decoration: boxDecoration(
+                      backgroundColor: const Color(0xff6A0030),
+                      borderRadius: 62
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      "Pending".textMedium(color: Colors.white,fontSize: 14),
+
+                      5.horizontalSpace(),
+
+                      SvgPicture.asset(Images.suggestedProject)
+
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  height: screenHeight(),
+                  margin: const EdgeInsets.all(6),
+                  decoration: boxDecoration(
+                      backgroundColor: const Color(0xff6A0030),
+                      borderRadius: 62
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      "Approved".textMedium(color: Colors.white,fontSize: 14),
+
+                      5.horizontalSpace(),
+
+                      SvgPicture.asset(Images.activeProject,)
+
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        Expanded(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 120.0),
+              child: customList(child: (index){
+                return const Padding(
+                  padding: EdgeInsets.only(right: 20.0,left: 10),
+                  child: MCCApplicationScreen(bottom: 25,),
+                );}),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
